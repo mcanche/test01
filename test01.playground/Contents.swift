18c77596564cfd3a2f388df9448d5def444758b5
mcanche <mcanche@gmail.com>
2015-06-12 18:17:05 -0500

    Test01
    playground - tutorial 01

---
+++ test01.playground/Contents.swift
@@ -0,0 +1,23 @@
+// +-------------+
+// | video 1     |
+// +-------------+
+
+
+
+import UIKit
+
+var name = "Martin"
+var title = "Sr"
+
+var returnValue = "Hola \(title) \(name)"
+
+var strongName:String = "Martin"
+
+var score = 125
+var mul = 4
+var output = "Your score is: \(score * mul)"
+
+
+let player = "Martin"
+
+let PUBLIC_ONS = "http://miweb.com"
